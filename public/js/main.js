e40fcc70309e8a51851e92a12828db61b2f67d65
Paul Grever <paulgrever@gmail.com>
2017-03-13 00:03:26 -0600

    fixed time issue where before midnight

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-  var currentDate = new Date;
+  var currentDate = new Date();
   var currentTime = currentDate.getHours() + 1;
+  if (currentTime <= 9) {
+    currentTime = "0" + currentTime;
+  }
   $('#time').val(currentTime + ':00');
 });
